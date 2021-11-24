2854125bb6aff7f518ca700ae841da051fb5890e
Shade St Jude <shade.st.jude84@gmail.com>
2021-11-24 21:31:55 +1030

    Create Bools.c

---
+++ Bools.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main()
+{
+    bool a = true;
+    bool b = false;
+    printf("a (TRUE): %d\n", a);
+    printf("b (FALSE): %d\n", b);
+
+    return EXIT_SUCCESS;
+}
